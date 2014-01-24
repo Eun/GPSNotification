58b942f57367b0cd0869d9a5abd73ed24c929353
Eun <eun@su.am>
2014-01-24 17:53:03 +0100

    * added posibility to decide where the icon should be displayed. * Relase 1.2

--- src/eun/xposed/gpsnotification/GPSNotification.java
+++ src/eun/xposed/gpsnotification/GPSNotification.java
@@ -13,6 +13,8 @@ import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
@@ -20,127 +22,294 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class GPSNotification  extends BroadcastReceiver implements IXposedHookLoadPackage,  IXposedHookZygoteInit, IXposedHookInitPackageResources  {
 	
-    public static final String GPS_ENABLED_CHANGE_ACTION =
-            "android.location.GPS_ENABLED_CHANGE";
-    public static final String GPS_FIX_CHANGE_ACTION =
-            "android.location.GPS_FIX_CHANGE";
-    public static final String EXTRA_GPS_ENABLED = "enabled";
-    
-    private static final int GPS_NOTIFICATION_ID = 374203-122084;
-    
-        
-	private Context mContext;
+	// Constants
+	public static final String PKG = "eun.xposed.gpsnotification";
+	public static final String ACTION_SETTINGS_CHANGED = PKG + ".changed";
+	private static final String GPS_ENABLED_CHANGE_ACTION = "android.location.GPS_ENABLED_CHANGE";
+	private static final String GPS_FIX_CHANGE_ACTION = "android.location.GPS_FIX_CHANGE";
+	private static final String EXTRA_GPS_ENABLED = "enabled";
+	private static final int GPS_NOTIFICATION_ID = 374203-122084;
+	private static final String LOCATION_STATUS_ICON_PLACEHOLDER = "location";
 	
 	private static String MODULE_PATH = null;
-	
+	private Class<?> LocationControllerClass;
+	private Class<?> QuickSettingsModelClass;
+	private Context mContext;
 	private NotificationManager nm;
 	
-	int stat_sys_gps_on, stat_sys_gps_acquiring, stat_sys_gps_acquiring_anim, gps_notification_found_text, gps_notification_searching_text;
+	int stat_sys_gps_on, stat_sys_gps_off, stat_sys_gps_on_right, // drawables
+		stat_sys_gps_acquiring_anim, stat_sys_gps_acquiring_anim_right, // animations
+		gps_notification_found_text, gps_notification_searching_text, accessibility_location_active; // texts
 	
+	//InitPackageResourcesParam resparam;
+		
+	public enum GPSIconPosition
+	{
+		NONE,
+		LEFT,
+		RIGHT;
+
+	    public static GPSIconPosition fromInteger(int value) {
+	    	switch(value) {
+	        case 1:
+	            return LEFT;
+	        case 2:
+	            return RIGHT;
+	        default:
+	        	return NONE;
+	    	}
+	    }
+
+	    public static int getValue(GPSIconPosition value) {
+	    	switch(value) {
+		        case LEFT:
+		            return 1;
+		        case RIGHT:
+		            return 2;
+		        default:
+		        	return 0;
+	        }
+	    }
+	}
+
+	public static XSharedPreferences prefs;
+	
+	private Object mStatusBarManager;
+	
+	Object mLocationTile, mLocationState, mLocationCallback;
 	
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+		 
 		if (!lpparam.packageName.equals("com.android.systemui"))
 	        return;
 		
+		
 		if (Build.VERSION.SDK_INT >= 19)
 		{
-			final Class<?> LocationControllerClass =  XposedHelpers.findClass("com.android.systemui.statusbar.policy.LocationController", lpparam.classLoader);
-			
+			LocationControllerClass = QuickSettingsModelClass = null;
+			prefs = new XSharedPreferences(PKG);
+			LocationControllerClass = XposedHelpers.findClass("com.android.systemui.statusbar.policy.LocationController", lpparam.classLoader);
 			XposedBridge.hookAllConstructors(LocationControllerClass, new XC_MethodHook() {
 				 @Override
-	             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-	                    
-	                     mContext = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
-	                     IntentFilter filter = new IntentFilter();
-	                     filter.addAction(GPS_ENABLED_CHANGE_ACTION);
-	                     filter.addAction(GPS_FIX_CHANGE_ACTION);
-	                     mContext.registerReceiver(GPSNotification.this, filter);
-	                     nm = (NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-	             }
-			});
+				 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+					 mContext = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
+		             IntentFilter filter = new IntentFilter();
+		             filter.addAction(GPS_ENABLED_CHANGE_ACTION);
+		             filter.addAction(GPS_FIX_CHANGE_ACTION);
+		             filter.addAction(ACTION_SETTINGS_CHANGED);
+		             mContext.registerReceiver(GPSNotification.this, filter);
+	                 nm = (NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+	                 mStatusBarManager = XposedHelpers.getObjectField(param.thisObject, "mStatusBarManager");
+	                }
+				 });
+			XposedHelpers.findAndHookMethod(LocationControllerClass, "refreshViews", XC_MethodReplacement.DO_NOTHING);
+
+			if (LocationControllerClass == null)
+			{
+				XposedBridge.log("GPSNotification: LocationController not found! Could not apply Notification.");
+			}
 			
-			XposedHelpers.findAndHookMethod(LocationControllerClass, "refreshViews", new XC_MethodHook() {
-				@Override
-	    		protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-	    			XposedHelpers.setBooleanField(param.thisObject, "mAreActiveLocationRequests", false);
-	    		}
-			});
-			
-			
+			// TODO: animation in QuickSettings
+			/*
+			if (LocationControllerClass)
+			{
+				QuickSettingsModelClass = XposedHelpers.findClass("com.android.systemui.statusbar.phone.QuickSettingsModel", lpparam.classLoader);
+				XposedHelpers.findAndHookMethod(QuickSettingsModelClass, "onLocationSettingsChanged", boolean.class, new XC_MethodHook() {
+					 @Override
+			            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						 mLocationTile = XposedHelpers.findField(QuickSettingsModelClass, "mLocationTile");
+						 mLocationState = XposedHelpers.findField(QuickSettingsModelClass, "mLocationState");
+						 mLocationCallback = XposedHelpers.findField(QuickSettingsModelClass, "mLocationCallback");
+						 if ((Boolean)param.args[0] == false)
+						 {
+							 XposedHelpers.setObjectField(mLocationState, "iconId", stat_sys_gps_off);
+							 XposedHelpers.callMethod(mLocationCallback, "refreshView", mLocationTile, mLocationState);
+						 }
+						}
+						});
+			}
+			if (QuickSettingsModelClass == null)
+			{
+				XposedBridge.log("GPSNotification: QuickSettingsModel not found! Could not apply animation in Quicksettings.");
+			}
+			*/		
+		}
+		else
+		{
+			LocationControllerClass = null;
 		}
 	}
 	
 	@Override
 	public void initZygote(StartupParam startupParam) throws Throwable {
-		MODULE_PATH = startupParam.modulePath;
+		if (Build.VERSION.SDK_INT >= 19)
+		{
+			MODULE_PATH = startupParam.modulePath;
+		}
 	}
 
+	
 	@Override
 	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
 		if (!resparam.packageName.equals("com.android.systemui"))
 			return;
 
-		XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res);
-		stat_sys_gps_on = resparam.res.addResource(modRes, R.drawable.stat_sys_gps_on);
-		stat_sys_gps_acquiring = resparam.res.addResource(modRes, R.drawable.stat_sys_gps_acquiring);
-		stat_sys_gps_acquiring_anim = resparam.res.addResource(modRes, R.drawable.stat_sys_gps_acquiring_anim);
-		gps_notification_searching_text = resparam.res.getIdentifier("gps_notification_searching_text", "string", resparam.packageName);
-		gps_notification_found_text  = resparam.res.getIdentifier("gps_notification_found_text", "string", resparam.packageName);
+	
+		if (Build.VERSION.SDK_INT >= 19 && LocationControllerClass != null)
+		{
+			XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res);
+			stat_sys_gps_on = resparam.res.addResource(modRes, R.drawable.stat_sys_gps_on);
+			stat_sys_gps_on_right = resparam.res.addResource(modRes, R.drawable.stat_sys_gps_on_right);
+			stat_sys_gps_off  = resparam.res.addResource(modRes, R.drawable.stat_sys_gps_off);
+			
+			stat_sys_gps_acquiring_anim = resparam.res.addResource(modRes, R.drawable.stat_sys_gps_acquiring_anim);
+			stat_sys_gps_acquiring_anim_right = resparam.res.addResource(modRes, R.drawable.stat_sys_gps_acquiring_anim_right);
+			
+			gps_notification_searching_text = resparam.res.getIdentifier("gps_notification_searching_text", "string", resparam.packageName);
+			gps_notification_found_text  = resparam.res.getIdentifier("gps_notification_found_text", "string", resparam.packageName);
+			accessibility_location_active = resparam.res.getIdentifier("accessibility_location_active", "string", resparam.packageName);
+			
+			// replace whole resource (e.g. for tiles)
+			
+			// Just in case QuickSettingsModel is not found,
+			// we can do it a bit dirty and replace the resources at runtime.
+			// Not fine, but works.
+			if (QuickSettingsModelClass == null)
+			{
+				resparam.res.setReplacement("com.android.systemui", "drawable", "ic_qs_location_on", modRes.fwd(R.drawable.stat_sys_gps_on));
+				resparam.res.setReplacement("com.android.systemui", "drawable", "ic_qs_location_off", modRes.fwd(R.drawable.stat_sys_gps_off));
+				//this.resparam = resparam;
+			}
+		}
+		
 		
 	}
 	
 	
-	
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		
+		GPSIconPosition IconPos;
 		final String action = intent.getAction();
+		 if (action.equals(ACTION_SETTINGS_CHANGED))
+		 {
+			 IconPos =  GPSIconPosition.fromInteger(Integer.parseInt(intent.getStringExtra("iconposition")));
+	
+			 // If pos has changed to none: remove all icons / notifications
+			 if (IconPos == GPSIconPosition.NONE)
+			 {
+				 nm.cancel(GPS_NOTIFICATION_ID);
+				 XposedHelpers.callMethod(mStatusBarManager, "removeIcon", LOCATION_STATUS_ICON_PLACEHOLDER);
+				 return;
+			 }
+			 // remove right
+			 else if (IconPos == GPSIconPosition.LEFT)
+			 {
+				 XposedHelpers.callMethod(mStatusBarManager, "removeIcon", LOCATION_STATUS_ICON_PLACEHOLDER);
+			 }
+			 // remove left
+			 else if (IconPos == GPSIconPosition.RIGHT)
+			 {
+				 nm.cancel(GPS_NOTIFICATION_ID);
+			 }
+			 // redraw
+		 }
+		 else
+		 {
+			 IconPos = GPSIconPosition.fromInteger(Integer.parseInt(prefs.getString("iconposition", String.valueOf(GPSIconPosition.getValue(GPSIconPosition.LEFT)))));
+		 }
+		
+		
+		if (IconPos == GPSIconPosition.NONE)
+		{
+			return;
+		}
+		
+		
+		
         final boolean enabled = intent.getBooleanExtra(EXTRA_GPS_ENABLED, false);
 
         boolean visible;
-        int iconId, textResId;
+        int textResId, icon;
 
         if (action.equals(GPS_FIX_CHANGE_ACTION) && enabled) {
             // GPS is getting fixes
-            iconId = stat_sys_gps_on;
+        	 if (IconPos == GPSIconPosition.LEFT)
+        		 icon = stat_sys_gps_on;
+        	 else
+        		 icon = stat_sys_gps_on_right;
             textResId = gps_notification_found_text;
             visible = true;
         } else if (action.equals(GPS_ENABLED_CHANGE_ACTION) && !enabled) {
             // GPS is off
             visible = false;
-            iconId = textResId = 0;
+            icon = textResId = 0;
         } else {
             // GPS is on, but not receiving fixes
-            iconId = stat_sys_gps_acquiring_anim;
+        	if (IconPos == GPSIconPosition.LEFT)
+        		icon = stat_sys_gps_acquiring_anim;
+        	else
+        		icon = stat_sys_gps_acquiring_anim_right;
             textResId = gps_notification_searching_text;
             visible = true;
         }
         
-        try {
-            if (visible) {
-                Intent gpsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                gpsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, gpsIntent, 0);
-
-                Notification n = new Notification.Builder(mContext)
-                    .setSmallIcon(iconId)
-                    .setContentTitle(mContext.getText(textResId))
-                    .setOngoing(true)
-                    .setContentIntent(pendingIntent)
-                    .build();
-
-                // Notification.Builder will helpfully fill these out for you no matter what you do
-                n.tickerView = null;
-                n.tickerText = null;
-                
-                
-                nm.notify(GPS_NOTIFICATION_ID, n);
-                 
-            } else {
-            	nm.cancel(GPS_NOTIFICATION_ID);
-            }
-        } catch (Exception ex) {
-            // well, it was worth a shot
+        if (IconPos == GPSIconPosition.LEFT)
+        {
+	        try {
+	            if (visible) {
+	                Intent gpsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+	                gpsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, gpsIntent, 0);
+	
+	                Notification n = new Notification.Builder(mContext)
+	                    .setSmallIcon(icon)
+	                    .setContentTitle(mContext.getText(textResId))
+	                    .setOngoing(true)
+	                    .setContentIntent(pendingIntent)
+	                    .build();
+	
+	                // Notification.Builder will helpfully fill these out for you no matter what you do
+	                n.tickerView = null;
+	                n.tickerText = null;
+	                
+	                
+	                nm.notify(GPS_NOTIFICATION_ID, n);
+	                 
+	            } else {
+	            	nm.cancel(GPS_NOTIFICATION_ID);
+	            }
+	        } catch (Exception ex) {
+	            // well, it was worth a shot
+	        }
         }
+        else if (IconPos == GPSIconPosition.RIGHT)
+		{
+			 if (visible)
+			 {
+				XposedHelpers.callMethod(mStatusBarManager, "setIcon", LOCATION_STATUS_ICON_PLACEHOLDER, icon, 0, mContext.getString(accessibility_location_active));
+			 }
+			 else
+			 {
+				XposedHelpers.callMethod(mStatusBarManager, "removeIcon", LOCATION_STATUS_ICON_PLACEHOLDER);
+			 }
+		}
+        
+        /*if (QuickSettingsModelClass == null)
+        {
+        	XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res);
+        	if (icon == stat_sys_gps_acquiring_anim || icon == stat_sys_gps_acquiring_anim_right)	
+        	{
+        		resparam.res.setReplacement("com.android.systemui", "drawable", "ic_qs_location_on",  modRes.fwd(R.drawable.stat_sys_gps_acquiring_anim));
+        	}
+        	else
+        	{
+        		resparam.res.setReplacement("com.android.systemui", "drawable", "ic_qs_location_on",  modRes.fwd(R.drawable.stat_sys_gps_on));
+        	}
+        }*/
 	}
+	
+
+
 }
---
+++ src/eun/xposed/gpsnotification/SettingsActivity.java
@@ -0,0 +1,65 @@
+package eun.xposed.gpsnotification;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.List;
+
+public class SettingsActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		getPreferenceManager().setSharedPreferencesMode(Context.MODE_WORLD_READABLE);
+		addPreferencesFromResource(R.xml.pref_general);
+		bindPreferenceSummaryToValue(findPreference("iconposition"));
+	}
+	
+	
+	private Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object value) {
+			String stringValue = value.toString();
+			if (preference instanceof ListPreference) {
+				// For list preferences, look up the correct display value in
+				// the preference's 'entries' list.
+				ListPreference listPreference = (ListPreference) preference;
+				int index = listPreference.findIndexOfValue(stringValue);
+
+				// Set the summary to reflect the new value.
+				preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+				
+				Intent intent = new Intent();
+				intent.setAction(GPSNotification.ACTION_SETTINGS_CHANGED);
+				intent.putExtra(preference.getKey(), stringValue);
+				sendBroadcast(intent);
+				
+			}
+			return true;
+		}
+	};
+
+	
+	private void bindPreferenceSummaryToValue(Preference preference) {
+		preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+		
+		sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), "1"));
+	}
+}
